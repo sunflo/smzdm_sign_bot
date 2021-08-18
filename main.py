e4817ce826ddbe5fae85c38c399570ad094acac7
floody <huangxiaozheng@kedacom.com>
2021-08-18 07:37:35 +0000

    update main.py.

--- main.py
+++ main.py
@@ -6,8 +6,36 @@
 import requests,os
 from sys import argv
 
-import config
+ 
 from utils.serverchan_push import push_to_wechat
+
+"""
+请求头
+"""
+DEFAULT_HEADERS = {
+        'Accept': '*/*',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'zh-CN,zh;q=0.9',
+        'Connection': 'keep-alive',
+        'Host': 'zhiyou.smzdm.com',
+        'Referer': 'https://www.smzdm.com/',
+        'Sec-Fetch-Dest': 'script',
+        'Sec-Fetch-Mode': 'no-cors',
+        'Sec-Fetch-Site': 'same-site',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36',
+        }
+
+
+"""
+调试用 COOKIE
+"""
+TEST_COOKIE = ''
+
+
+"""
+调试用 SERVERCHAN_SECRETKEY
+"""
+SERVERCHAN_SECRETKEY = ''
 
 class SMZDM_Bot(object):
     def __init__(self):
